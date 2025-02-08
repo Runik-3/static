7dddce6f3dbe9d21b5e3fd266dff068893ad9a0e
matteomiceli <omatteomiceli@gmail.com>
2025-02-08 14:07:55 -0800

    feat: refresh design
    Clean and simple. Plans:
    - clean up desktop
    - update how it works section to follow this figma https://www.figma.com/design/m0jYQkpnkv0st9BH2fEpnq/Runik-2.0?node-id=1032-34&t=CfdXvfglVr3KrmEK-0
      - include animations

--- parallax.js
+++
@@ -1,8 +0,0 @@
-const foregroundPlanet = document.getElementById('planet1')
-const backgroundPlanet = document.getElementById('planet2')
-
-window.addEventListener('scroll', () => {
-  // 128px == 8rem
-  foregroundPlanet.style.transform = `translateY(${128 - (window.scrollY * .50)}px)`
-  backgroundPlanet.style.transform = `translateY(-${window.scrollY * .10}px)`
-})
