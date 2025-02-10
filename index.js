1635ce5824938bbaec40f4eb66abfc0aa683c466
matteomiceli <omatteomiceli@gmail.com>
2025-02-10 00:06:28 -0800

    feat: how it works section

---
+++ index.js
@@ -0,0 +1,15 @@
+const type = new Typewriter("#wiki-input", { loop: true, delay: 40, cursor: "" })
+type.typeString('https://lotr.fandom.com').pauseFor(3000).deleteAll(1).start()
+
+const converText = document.getElementById('convert-text')
+function scramble(el, text) {
+
+
+}
+
+function ascii() {
+  // min and max ascii codes
+  const min = 32
+  const max = 127
+
+}
